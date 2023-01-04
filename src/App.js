6d685d0242d07881339ae0633c0ea6956262f752
bnros <b.ros92@gmail.com>
2023-01-03 16:12:11 -0800

    completed user stories 1-4

--- src/App.js
+++ src/App.js
@@ -3,13 +3,57 @@ import Square from './components/Square'
 import './App.css'
 
 const App = () => {
-  const [squares, setSquares] = useState(Array(9).fill(null))
+  // const [squares, setSquares] = useState(Array(9).fill(null)) ([
+    const [board, setBoard] = useState([
+  "▪️",
+  "▪️",
+  "▪️",
+  "▪️",
+  "▪️",
+  "▪️",
+  "▪️",
+  "▪️",
+  "▪️"
+  ])
+
+  // const [xLocation, setXLocation] = useState(Math.floor(Math.random() * board.length))
+
+  // const [oLocation, setOLocation] = useState(Math.floor(Math.random() * board.length))
+
+  // ^^ did not work
+
+  const [currentPlayer, setCurrentPlayer] = useState("❌")
+
+  const handleGamePlay = (clickedSquare) => {
+    console.log(clickedSquare)
+    let updateBoard = [...board]
+    if(updateBoard[clickedSquare] === "▪️") {
+      updateBoard[clickedSquare] = currentPlayer
+      setBoard(updateBoard)
+      setCurrentPlayer(
+        currentPlayer === "❌" ? "⭕️" : "❌")
+          
+      
+
+
+      
+    }
+
+  
+  }
 
   return (
-    <>
+   <>
       <h1>Tic Tac Toe</h1>
-      <Square />
-    </>
+      <div className="board">
+        {board.map((square, index) => {
+        return (
+          <Square square={square} index={index} handleGamePlay={handleGamePlay}/>
+        )
+        
+  })}
+        </div>
+      </>
   )
 }
 
